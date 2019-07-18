56899559ffd343a6031149a92a6b78eb96cd9c84
jesusmontano <jesusmontano96@gmail.com>
2019-07-18 10:38:17 -0700

    Create draw method for moving object

--- src/moving_object.js
+++ src/moving_object.js
@@ -1,3 +1,5 @@
+import { __esModule } from "../dist/main";
+
 class MovingObject {
     constructor(options) {
         this.pos = options.pos;
@@ -5,4 +7,16 @@ class MovingObject {
         this.radius = options.radius;
         this.color = options.color;
     }
+
+    draw(ctx) {
+        ctx.fillStyle = this.color;
+
+        ctx.beginPath();
+        ctx.arc(
+            this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true
+        );
+        ctx.fill();
+    }
 }
+
+module.exports = MovingObject;
