b9f771e973bb4492a9401765a3e26caccc144a6d
jesusmontano <jesusmontano96@gmail.com>
2019-07-18 12:04:33 -0700

    Create draw method for game class

--- src/game.js
+++ src/game.js
@@ -21,8 +21,23 @@ class Game {
             Game.DIM_Y * Math.random()
         ];
     }
+
+    allObjects() {
+        return [].concat(this.dribblers, this.defenders);
+    }
+
+    draw(ctx) {
+        ctx.clearRect(0, 0, Game.DIM_X, Game.DIM_Y);
+        ctx.fillStyle = Game.BG_COLOR;
+        ctx.fillRect(0, 0, Game.DIM_X, Game.DIM_Y);
+
+        this.allObjects().forEach((object) => {
+            object.draw(ctx);
+        });
+    }
 }
 
+Game.BG_COLOR = "#000000";
 Game.DIM_X = 1000;
 Game.DIM_Y = 600;
 // Game.FPS = 32;
